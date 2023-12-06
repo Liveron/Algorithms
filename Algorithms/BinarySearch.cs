08033f998e7cc1ccbf21ffbe4889b341461416fd
Leni <laurarindo@gmail.com>
2023-12-06 23:52:11 +0300

    Init commit

---
+++ Algorithms/BinarySearch.cs
@@ -0,0 +1,27 @@
+﻿namespace Algorithms;
+public class BinarySearch
+{
+    static int Invoke(int[] arrToSearch, int itemToSearch)
+    {
+        int low = 0;
+        int high = arrToSearch.Length - 1;
+        int mid;
+        int guessPosition;
+
+        while (low <= high)
+        {
+            mid = (low + high) / 2;
+            guessPosition = arrToSearch[mid];
+
+            if (guessPosition == itemToSearch)
+                return mid;
+
+            else if (guessPosition > itemToSearch)
+                high = mid - 1;
+
+            else low = mid + 1;
+        }
+
+        return -1;
+    }
+}
