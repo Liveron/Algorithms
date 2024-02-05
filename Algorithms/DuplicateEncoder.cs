7ab481c43db298911c9a6e0bad9ca6dd7491498c
Liveron <minigalimov.lenar@yandex.ru>
2024-02-05 13:41:20 +0300

    DuplicateEncoder algorithm added

---
+++ Algorithms/DuplicateEncoder.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace Algorithms
+{
+    public static class DuplicateEncoder
+    {
+        public static string Invoke(string word)
+        {
+            string lower = word.ToLower();
+            var counts = new Dictionary<char, int>();
+
+            foreach (var character in lower)
+            {
+                if (counts.ContainsKey(character))
+                    counts[character]++;
+                else
+                    counts.Add(character, 1);
+            }
+
+            var resultString = new StringBuilder();
+            foreach (var character in lower)
+            {
+                if (counts[character] == 1)
+                    resultString.Append('(');
+                else
+                    resultString.Append(')');
+            }
+
+            return resultString.ToString();
+        }
+    }
+}
